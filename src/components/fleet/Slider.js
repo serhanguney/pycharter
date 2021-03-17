172366e49c63050fc33b2c7c0ecadd92198c15f1
yuivae <gnyserhan@gmail.com>
2021-03-17 10:57:30 +0300

    .

--- src/components/fleet/Slider.js
+++ src/components/fleet/Slider.js
@@ -1,94 +1,93 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import ReactDom from "react-dom";
 import useSlider from "../../hooks/useSlider";
-import gsap, { Power3 } from "gsap";
 import Modal from "../Modal";
+import { animate, motion, useMotionValue } from "framer-motion";
+import { Portfolio } from "../../context";
 
 export default function Slider({ images }) {
   //create a state and fetch its reducers using custom hook
   const { slide, nextSlide, previousSlide } = useSlider();
+
+  const {
+    dimensions: { width },
+  } = useContext(Portfolio);
+
   let gallery = useRef(null);
+  const slideMotionValue = useMotionValue(0);
+  const motionTransition = { duration: 0.8, ease: [0.79, 0.14, 0.15, 0.86] };
+
+  const imageVariants = {
+    initial: { scale: 1 },
+    animate: (i) => ({
+      scale: (i - slide.no) * 0.1 + 1,
+      transition: motionTransition,
+    }),
+  };
 
   //can't use the reducer for modal because it triggers the useeffect unnecessarily
   const [modal, setModal] = useState(false);
-
-  //for modal animation
-  let modalPosition = useRef(null);
-
-  //create tween references to get isActive methods.
-  let tweenToNext = useRef(null);
-  let tweenToPrevious = useRef(null);
 
   //handle clicks using reducers.
   //Notice the slide.no conditions; without them at the edge of limits the animations get buggy.
   //also without them the slidebuttons fall off the edge.
   //prevent double slides using isActive of gsap
   function handleClick(e) {
-    if (
-      !tweenToNext.current.isActive() &&
-      !tweenToPrevious.current.isActive()
-    ) {
+    if (!slideMotionValue.isAnimating()) {
       if (e.target.name === "previous" && slide.no > 0) {
         previousSlide(0);
+        const width = gallery.children[0].getBoundingClientRect().width;
+        const previousValue = slideMotionValue.get();
+
+        // const scaleRatio = Array.from(gallery.children).map((item,index)=> )
+        animate(slideMotionValue, previousValue + width, motionTransition);
       } else if (e.target.name === "next" && slide.no < images.length - 1) {
         nextSlide(images.length);
+
+        const width = gallery.children[0].getBoundingClientRect().width;
+        const previousValue = slideMotionValue.get();
+        animate(slideMotionValue, previousValue - width, motionTransition);
       }
     }
   }
 
   function handleToggle() {
-    modalPosition.current = {
-      height: gallery.children[slide.no].getBoundingClientRect().height,
-      width: gallery.children[slide.no].getBoundingClientRect().width,
-      top: gallery.children[slide.no].getBoundingClientRect().top,
-      left: gallery.children[slide.no].getBoundingClientRect().left,
-    };
     setModal(true);
   }
   //handle sliding based on limits and direction. Use gallery ref to fetch children and grandchildren of parent.
   //children for sliding, grandchildren for scaling
   useEffect(() => {
-    tweenToNext.current = gsap.to(gallery.children, {
-      x: `-=100%`,
-      ease: Power3.easeInOut,
-      duration: 1,
-      paused: true,
-    });
-    tweenToPrevious.current = gsap.to(gallery.children, {
-      x: `+=100%`,
-      ease: Power3.easeInOut,
-      duration: 1,
-      paused: true,
-    });
-    if (slide.no < images.length && slide.moveTo === "next") {
-      //prevent interruptions using isActive
-      if (!tweenToNext.current.isActive()) {
-        tweenToNext.current.play();
-        gsap.from(gallery.children[slide.no].children, {
-          scale: 1.3,
-          ease: Power3.easeInOut,
-          duration: 1,
-        });
-      }
-    } else if (slide.no > -1 && slide.moveTo === "previous") {
-      //prevent interruptions using isActive
-      if (!tweenToPrevious.current.isActive()) {
-        tweenToPrevious.current.play();
-        gsap.from(gallery.children[slide.no].children, {
-          scale: 1.3,
-          ease: Power3.easeInOut,
-          duration: 1,
-        });
-      }
+    if (slideMotionValue.hasAnimated) {
+      const width = gallery.children[0].getBoundingClientRect().width;
+      slideMotionValue.prev < 0
+        ? animate(slideMotionValue, width * -slide.no, motionTransition)
+        : animate(
+            slideMotionValue,
+            width * slideMotionValue * slide.no,
+            motionTransition
+          );
     }
-  }, [slide]);
+  }, [width]);
   return (
     <div className="slider">
       <div ref={(el) => (gallery = el)} className="gallery">
         {images.map((image, index) => (
-          <div key={index} className="image-container" onClick={handleToggle}>
-            <img key={index} src={image} alt="boat" />
-          </div>
+          <motion.div
+            style={{ x: slideMotionValue }}
+            key={index}
+            className="image-container"
+            onClick={handleToggle}
+          >
+            <motion.img
+              custom={index}
+              key={index}
+              src={image}
+              alt="boat"
+              initial="intial"
+              animate="animate"
+              variants={imageVariants}
+            />
+          </motion.div>
         ))}
       </div>
       <div className="button-container">
@@ -105,11 +104,7 @@ export default function Slider({ images }) {
         ))}
       </div>
       {ReactDom.createPortal(
-        <Modal
-          open={modal}
-          close={() => setModal(false)}
-          position={modalPosition.current}
-        >
+        <Modal open={modal} close={() => setModal(false)}>
           <div className="image-container">
             <img src={images[slide.no]} alt="boat" />
           </div>
